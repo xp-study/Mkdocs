1384f944d853efc094a144b23ece1c3798a59ce8
Administrator <noreply@drone>
2022-11-27 20:50:32 +0800

    删除不必要的文件

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM harbor.wsh-study.com/public/mkdocs-base:latest
-
-ADD ./ /opt/Mkdocs
-
-WORKDIR /opt/Mkdocs
-
-ENTRYPOINT ["mkdocs serve --dev-addr=0.0.0.0:10000"]
--- start.sh
+++ start.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
-result=$(docker ps | grep "mkdocs")
-if [[ "$result" != "" ]];then
-    echo "stop mkdocs" 
-    docker stop mkdocs
-fi
 
-result1=$(docker ps -a | grep "mkdocs")
-if [[ "$result1" != "" ]];then
-    echo "rm mkdocs" 
-    docker rm mkdocs
-fi
 
-result2=$(docker images | grep "mkdocs")
-if [[ "$result2" != "" ]];then
-    echo "rmi mkdocs" 
-    docker rmi mkdocs
-fi
-
-docker build . --tags mkdocs:latest
-
-docker run -itd -p 10000:10000 --name mkdocs mkdocs:latest
-
-rm -rf /opt/mkdocs/*
+mkdocs serve --dev-addr=0.0.0.0:10000 &

a40e95bc37f18d237f8eb07427e77afa0cd1964a
Administrator <noreply@drone>
2022-11-28 03:22:41 +0800

    调整启动脚本

--- start.sh
+++ start.sh
@@ -5,19 +5,19 @@ containerName=local-mkdocs
 runningID=$(docker ps | grep $containerName | awk '{print $1}')
 if [[ "$runningID" != "" ]];then
     echo "stop $containerName" 
-    docker stop runningID
+    docker stop $runningID
 fi
 
 stoppedID=$(docker ps -a | grep $containerName | awk '{print $1}')
 if [[ "$stoppedID" != "" ]];then
     echo "rm $containerName" 
-    docker rm stoppedID
+    docker rm $stoppedID
 fi
 
 imageID=$(docker images | grep $containerName | awk '{print $3}')
 if [[ "$imageID" != "" ]];then
     echo "rmi $containerName" 
-    docker rmi imageID
+    docker rmi $imageID
 fi
 
 docker build . -t $containerName

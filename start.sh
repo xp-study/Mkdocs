c0ab4fdda34c2fc8d0f83ba2f2a7adc8eeb64289
Administrator <noreply@drone>
2022-11-28 03:16:47 +0800

    修改启动脚本

--- start.sh
+++ start.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-docker build . -t local-mkdocs
+containerName=local-mkdocs
 
-docker run -p 10000:10000 local-mkdocs /bin/sh  >/dev/null 2>&1 &
+runningID=$(docker ps | grep $containerName | awk '{print $1}')
+if [[ "$runningID" != "" ]];then
+    echo "stop $containerName" 
+    docker stop runningID
+fi
+
+stoppedID=$(docker ps -a | grep $containerName | awk '{print $1}')
+if [[ "$stoppedID" != "" ]];then
+    echo "rm $containerName" 
+    docker rm stoppedID
+fi
+
+imageID=$(docker images | grep $containerName | awk '{print $3}')
+if [[ "$imageID" != "" ]];then
+    echo "rmi $containerName" 
+    docker rmi imageID
+fi
+
+docker build . -t $containerName
+
+docker run -p 10000:10000 $containerName /bin/sh  >/dev/null 2>&1 &
